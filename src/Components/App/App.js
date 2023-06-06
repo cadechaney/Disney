ed6bd535d194a8d513a92123454eb18b723f7e9f
Cade Chaney <cadechaney@live.com>
2023-06-06 13:49:11 -0500

    Add disney logo to assets folder

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Header from '../Header/Header'
+
 
 function App() {
   return (
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Header() {
+
+
+  return (
+    <>
+      <h1>Disney Character Discovery</h1>
+    </>
+  )
+}
+
+export default Header
