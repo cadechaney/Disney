54c78463b838404a9db7401bad6d2f396cd39e8a
Cade Chaney <cadechaney@live.com>
2023-06-06 14:22:27 -0500

    Add main page css and js files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Header/Header'
+import MainPage from '../MainPage/MainPage'
 import './App.css'
 
 function App() {
   return (
     <>
       <Header />
+      <MainPage />
     </>
   );
 }
---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MainPage.css'
+
+function MainPage() {
+
+  return (
+    <> 
+    <p>main Page</p>
+    </>
+  )
+}
+
+export default MainPage
+
