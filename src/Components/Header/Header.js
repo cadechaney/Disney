5dc7e92089634e347a6f52ec944cdd825d618273
Cade Chaney <cadechaney@live.com>
2023-06-06 14:17:06 -0500

    Add basic layout to static header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './Header.css'
 import Logo from '../../assets/disneyLogo.png'
 
 function Header() {
 
 
   return (
-    <>
-      <h1>Disney Character Discovery</h1>
-      <img src={Logo}></img>
-    </>
+    <div className='header'>
+      <h1 className='app-title'>Disney Character Discovery</h1>
+      <img src={Logo} alt='disney logo image/ home button' className='header-image'></img>
+    </div>
   )
 }
 
