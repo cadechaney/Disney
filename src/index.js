6cde24ff25d9b1d886e9272b351ddb51da7f4b76
Cade Chaney <cadechaney@live.com>
2023-06-06 22:49:21 -0500

    Finally fixed the router import error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App/App.js';
+// import { BrowserRouter as Router} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+// const router = <BrowserRouter>
+// <App />
+// </BrowserRouter>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +16,13 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+// ReactDOM.render(router, document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
