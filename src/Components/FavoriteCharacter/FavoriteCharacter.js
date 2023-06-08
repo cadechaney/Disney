f7325aed826c67c012c2c553e4b2feefb94204e0
Cade Chaney <cadechaney@live.com>
2023-06-08 12:23:24 -0500

    Add flip ability to main character cards

--- src/Components/FavoriteCharacter/FavoriteCharacter.js
+++ src/Components/FavoriteCharacter/FavoriteCharacter.js
@@ -2,21 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './FavoriteCharacter.css'
 
-function FavoriteCharacter({ id, films, tvShows, videoGames, allies, enemies, name, imageUrl, viewCharacter}) {
+function FavoriteCharacter({ id, films, tvShows, name, imageUrl, viewCharacter}) {
 
   return (
     <div className='favorite-character-card' id={id}>
-      <section className='card-inner'>
-        <div className='card-front'>
+      <section className='favorite-card-inner'>
+        <div className='favorite-card-front'>
           <img className='favorite-character-image' alt='Character Portrait' src={imageUrl}></img>
         </div>
         <div className='favorite-character-details'>
           <h2>Name: {name}</h2>
           <h4>Films: {films}</h4>
           <h4>TV Shows: {tvShows}</h4>
-          <h4>Video Games:{videoGames}</h4>
-          <h4>Allies: {allies}</h4>
-          <h4>Enemies: {enemies}</h4>
           <div>
             <Link to={`/${id}`} key={id}>
               <button onClick={() => viewCharacter(id)}>View Info</button>
