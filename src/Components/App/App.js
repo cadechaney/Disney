b332ce49a394f32e49ee9a11d821f63c9ef5e9f5
Cade Chaney <cadechaney@live.com>
2023-06-07 21:00:12 -0500

    Add Favorites js and css files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import Header from '../Header/Header'
 import MainPage from '../MainPage/MainPage'
 import CharacterDetails from '../CharacterDetails/CharacterDetails'
+// import Favorites from 
 import './App.css'
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
@@ -90,6 +91,7 @@ class App extends Component {
         <Header />
         <Switch>
           <Route path='/:id' render={({match}) => <CharacterDetails clickedCharacter={this.state.singleCharacterDetails} key={match.params.id} id={match.params.id} addFavorite={this.addFavorite} favoritesList={this.state.favorites} />} />
+          <Route path='/favorites' render={() => <Favorites />}/>
           <Route path='/' render={() => <MainPage charactersDetails={this.state.characters} viewCharacter={this.viewCharacter} />} />
         </Switch>
       </>
