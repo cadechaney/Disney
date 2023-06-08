de3de6d3bd1daace65aeb405f6b952106db3b3b4
Cade Chaney <cadechaney@live.com>
2023-06-08 12:42:14 -0500

    Add Oswald as the view favorites button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
 import Logo from '../../assets/disneyLogo.png'
+import oswald from '../../assets/Oswald_waving_at_camera.webp'
 
 function Header() {
 
@@ -11,12 +12,12 @@ function Header() {
       <h1 className='app-title'>Disney Character Discovery</h1>
       <div className='view-favorites'>
         <Link to={'/favorites'}>
-          <p>Image will be here</p>
+          <img src={oswald} alt='Oswald waving/ view favorites' title='View Favorites' className='view-favorites'></img>
         </Link>
       </div>
       <div className='header-image'>
         <Link to={'/'}>
-          <img src={Logo} alt='disney logo image/ home button' className='header-image'></img>
+          <img src={Logo} alt='disney logo image/ home button' title='View Home Page' className='header-image'></img>
         </Link>
       </div>
     </div>
