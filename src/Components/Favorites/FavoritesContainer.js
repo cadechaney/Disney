1faba55beb62070fd4fa4c38c3d0524cce4b8741
Cade Chaney <cadechaney@live.com>
2023-06-07 21:19:53 -0500

    Add new sad mickey gif

--- src/Components/Favorites/FavoritesContainer.js
+++ src/Components/Favorites/FavoritesContainer.js
@@ -1,11 +1,14 @@
 
 import React from 'react'
+// import sadMickey from '../../assets/sadMickey.webp'
+import sadMickey from '../../assets/mickeymouse-sad.gif'
 
 function FavoritesContainer({ favoritesDetails }) {
   console.log(favoritesDetails)
   if(favoritesDetails.length === 0) {
     return (
       <section className='no-favorites-container'>
+        <img src={sadMickey} alt='Sad Mickey'></img>
         <h1>You Have No Favorites</h1>
 
       </section>
