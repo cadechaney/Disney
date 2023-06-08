c15d418237b300261db25f971bd20f8fc27f7d6b
Cade Chaney <cadechaney@live.com>
2023-06-07 21:15:16 -0500

    Add sad mickey image

--- src/Components/Favorites/FavoritesContainer.js
+++ src/Components/Favorites/FavoritesContainer.js
@@ -1,11 +1,21 @@
 
 import React from 'react'
 
-function FavoritesContainer() {
-  
-  return (
-    <h1>Favorites Container</h1>
-  )
+function FavoritesContainer({ favoritesDetails }) {
+  console.log(favoritesDetails)
+  if(favoritesDetails.length === 0) {
+    return (
+      <section className='no-favorites-container'>
+        <h1>You Have No Favorites</h1>
+
+      </section>
+      )
+    } else {
+      return (
+      <h1>you favorited {favoritesDetails.length} character/s</h1>
+    )
+
+  }
 }
 
 export default FavoritesContainer
