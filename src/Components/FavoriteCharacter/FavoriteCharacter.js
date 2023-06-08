99f63ebe83553fe5c89de5b761eb4e2e2c079dfd
Cade Chaney <cadechaney@live.com>
2023-06-08 12:02:36 -0500

    Add characters are rendered to the favorites page

--- src/Components/FavoriteCharacter/FavoriteCharacter.js
+++ src/Components/FavoriteCharacter/FavoriteCharacter.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './FavoriteCharacter.css'
 
-function FavoriteCharacter() {
+function FavoriteCharacter({ id, films, tvShows, videoGames, allies, enemies, name, imageUrl}) {
 
   return (
-    <h1>Card</h1>
+    <div className='favorite-character-card' id={id}>
+      <section className='card-inner'>
+        <div className='card-front'>
+          <img className='favorite-character-image' alt='Character Portrait' src={imageUrl}></img>
+        </div>
+        <div className='favorite-character-details'>
+          <h2>Name: {name}</h2>
+          <h4>Films: {films}</h4>
+          <h4>TV Shows: {tvShows}</h4>
+          <h4>Video Games:{videoGames}</h4>
+          <h4>Allies: {allies}</h4>
+          <h4>Enemies: {enemies}</h4>
+        </div>
+      </section>
+    </div>
   )
 }
 
