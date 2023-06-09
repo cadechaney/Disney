af835d226b5ab21d4357081cba5b8f7cc24c674c
Cade Chaney <cadechaney@live.com>
2023-06-09 11:55:08 -0500

    Add proptypes to Character details component

--- src/Components/CharacterDetails/CharacterDetails.js
+++ src/Components/CharacterDetails/CharacterDetails.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import './CharacterDetails.css'
 import goldStar from '../../assets/Gold_Star.svg-1.png'
 import emptyStar from '../../assets/emptyStar.png'
 
 function CharacterDetails({clickedCharacter, addFavorite, favoritesList}) {
-console.log(clickedCharacter)
-// const obj = {}
+
 if(Object.keys(clickedCharacter).length === 0) {
   return (
     <h1>Click the Castle to View Other Characters</h1>
@@ -35,7 +35,50 @@ if(Object.keys(clickedCharacter).length === 0) {
       </section>
     </div>
   )
-}
+}}
+
+CharacterDetails.propTypes = {
+  clickedCharacter: PropTypes.shape({
+    _id: PropTypes.number,
+    films: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    imageUrl: PropTypes.string,
+    tvShows: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    videoGames: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    allies: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    enemies: PropTypes.arrayOf(
+      PropTypes.string
+    )
+  }),
+  addFavorite: PropTypes.func.isRequired,
+  favoritesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.number,
+    films: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    imageUrl: PropTypes.string,
+    tvShows: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    videoGames: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    allies: PropTypes.arrayOf(
+      PropTypes.string
+    ),
+    enemies: PropTypes.arrayOf(
+      PropTypes.string
+    )
+    })
+  )
 }
 
 export default CharacterDetails
