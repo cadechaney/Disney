5818b6107c7d886e6b891e136b91ff7b25179d3c
Cade Chaney <cadechaney@live.com>
2023-06-09 20:56:22 -0500

    Add tests for the mainpage

--- cypress/e2e/MainPage/spec.cy.js
+++ cypress/e2e/MainPage/spec.cy.js
@@ -5,7 +5,27 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000')
   })
 
-  it('passes', () => {
-    
+  it('should display the header', () => {
+    cy.get('.header').should('be.visible');
+  })
+
+  it('should contain the correct content', () => {
+    cy.get('.app-title').contains('Disney Character Discovery')
+    cy.get('.view-favorites').should('be.visible')
+    cy.get('.header-image').should('be.visible')
+  })
+
+  it('should have an image from the character', () => {
+    cy.get('.character-image').should('be.visible')
+  })
+
+  it('should have a view info button below it', () => {
+    cy.get('.more-info-button').first().should('be.visible')
+  })
+
+  it('should render an error page if there is one', () => {
+    cy.visit('http://localhost:3000/error')
+    cy.get('h1').contains('Click the Castle to View Other Characters')
+    .should('be.visible')
   })
 })
